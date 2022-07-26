82e33a277444b58a8c1f8bbdf3c464feb5aef758
Audrey Teles dos Santos <68817384+Audrey-Teles@users.noreply.github.com>
2022-07-26 02:11:00 -0300

    Web scraping e Home
    Criação do script Python para pegar o link de uma foto e o nome do personagem do site do Fandom de Ordem Paranormal e a parte da tela principal do jogo

---
+++ views.py
@@ -0,0 +1,13 @@
+from urllib.request import urlopen, Request
+from app import app
+from flask import render_template
+from web_scraping import getLink
+
+
+@app.get("/")
+def index():
+    nome,imagem = getLink()
+    return render_template("template.html",
+                           personagem=nome,
+                           imagem=imagem,
+                           blur=20)
---
+++ web_scraping.py
@@ -0,0 +1,30 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as bs
+from random import randrange
+
+def tratarHtml(url):
+    response = urlopen(url)
+    html = response.read()
+
+    # Tratamento do html
+    html = html.decode("utf-8")
+    replaced = " ".join(html.split()).replace("> <", "><")
+    return bs(replaced, 'html.parser')
+
+
+def getLink():
+    soup = tratarHtml("https://ordemparanormal.fandom.com/wiki/Personagens")
+
+    # Criando lista com os liks para cada página de personagens
+    lista = soup.findAll("div", {"class":"moldura-link"})
+
+    # Sorteia uma das páginas de personagens da lista para pegar o link
+    soup = tratarHtml("https://ordemparanormal.fandom.com" + lista[randrange(0, len(lista)-1)].a["href"])
+
+    # Lista as imagens de "perfil dos personagens"
+    lista = soup.findAll("img", {"class":"pi-image-thumbnail"})
+
+    imagem = lista[len(lista) -1]["src"].replace("static", "vignette")
+
+    # Retorna o nome do perfil e a imagem de "perfil" mais recente
+    return (soup.h1.getText(), imagem)
