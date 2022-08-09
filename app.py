da66da4b9a94b64fe04dd35f28f5195cce6d9086
Audrey Teles dos Santos <68817384+Audrey-Teles@users.noreply.github.com>
2022-08-09 12:19:41 -0300

    Função de pontos
    Correção da função que fornece a pontuação e todas as validações do jogo

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 from views import *
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- views.py
+++ views.py
@@ -4,19 +4,29 @@ from app import app
 from flask import flash, render_template, request, url_for, session
 from web_scraping import getLink
 from localStoragePy import localStoragePy
-
+import time
 nome = ""
 imagem = ""
 app.secret_key = 'super secret key'
 localStorage = localStoragePy('app', 'json')
+valor = 0
 @app.get("/")
 def index():
-    global nome, imagem
 
-    if imagem == "" and nome == "":
-        localStorage.setItem('pontos', 5)
-        localStorage.setItem('blur', 25)
-        nome, imagem = getLink()
+    if session.get('pontos') is None:
+        session['pontos'] = 5
+        session['blur'] = 25
+
+
+
+    global nome, imagem
+    global valor
+    if int(time.strftime("%H")) > 0 and valor ==0:
+
+            valor = valor + 1
+            session['pontos'] = 5
+            session['blur'] = 25
+            nome, imagem = getLink()
 
     return render_template("template.html",
                            personagem=nome,
@@ -32,10 +42,13 @@ def autenticar():
     entrada = " " + request.form['entrada'] + " "
     print(nome)
     if entrada.lower() == nome.lower():
+        session['blur'] = 0
         flash("Parabéns, você acertou!!")
+
     else:
+        session['pontos'] = session['pontos'] - 1
+        session['blur'] = session['blur'] - 5
         localStorage.setItem('pontos', int(localStorage.getItem('pontos'))-1)
         localStorage.setItem('blur', int(localStorage.getItem('blur'))-5)
-        flash("Errou.", "error")
 
     return redirect(url_for('index'))
