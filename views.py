a4974b6de1301b93ccfda83f1b8c44b0c8461f72
Audrey Teles dos Santos <68817384+Audrey-Teles@users.noreply.github.com>
2022-08-08 23:55:29 -0300

    template e rotas
    Adições na interface e rotas para verificação da pontuação

--- views.py
+++ views.py
@@ -1,15 +1,48 @@
 from urllib.request import urlopen, Request
+from werkzeug.utils import redirect
 from app import app
-from flask import render_template
+from flask import flash, render_template, request, url_for, session
 from web_scraping import getLink
+
+nome = ""
+imagem = ""
+
+class Usuario:
+    def __init__(self):
+        self.pontos = 5
+
+    def setPontos(self):
+        self.pontos =- 1
+
+    def getPontos(self):
+        return self.pontos
+
+user = Usuario()
 
 @app.get("/")
 def index():
-    nome,imagem = getLink()
+    global nome, imagem
+    if nome == "" and imagem == "":
+        session['blur'] = 25
+        session['pontos'] = 5
+        nome, imagem = getLink()
+
     return render_template("template.html",
                            personagem=nome,
                            imagem=imagem,
-                           blur=15)
+                           blur=10,
+                           pontos=5)
+
+
 @app.post('/autenticar')
 def autenticar():
-    return ""
+    entrada = " " + request.form['entrada'] + " "
+    print(nome)
+    if entrada.lower() == nome.lower():
+        flash("Parabéns, você acertou!!")
+    else:
+        session['pontos'] = session['pontos'] - 1
+        session['blur'] = session['blur'] - 5
+        flash("Errou.", "error")
+
+    return redirect(url_for('index'))
--- web_scraping.py
+++ web_scraping.py
@@ -1,6 +1,10 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
 from random import randrange
+import json
+
+from flask import url_for
+
 
 def tratarHtml(url):
     response = urlopen(url)
@@ -16,15 +20,15 @@ def getLink():
     soup = tratarHtml("https://ordemparanormal.fandom.com/wiki/Personagens")
 
     # Criando lista com os liks para cada página de personagens
-    lista = soup.findAll("div", {"class":"moldura-link"})
+    lista = soup.findAll("div", {"class": "moldura-link"})
 
     # Sorteia uma das páginas de personagens da lista para pegar o link
-    soup = tratarHtml("https://ordemparanormal.fandom.com" + lista[randrange(0, len(lista)-1)].a["href"])
+    soup = tratarHtml("https://ordemparanormal.fandom.com" + lista[randrange(0, len(lista) - 1)].a["href"])
 
     # Lista as imagens de "perfil dos personagens"
-    lista = soup.findAll("img", {"class":"pi-image-thumbnail"})
+    lista = soup.findAll("img", {"class": "pi-image-thumbnail"})
 
-    imagem = lista[len(lista) -1]["src"].replace("static", "vignette")
+    imagem = lista[len(lista) - 1]["src"].replace("static", "vignette")
 
     # Retorna o nome do perfil e a imagem de "perfil" mais recente
     return soup.h1.getText(), imagem
