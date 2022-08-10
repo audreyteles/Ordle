60ece553d2dcc64d9a2f25d68b4f0e78536fbcac
Audrey Teles dos Santos <68817384+Audrey-Teles@users.noreply.github.com>
2022-08-09 23:47:12 -0300

    Update views.py
    Testes de write do json

--- views.py
+++ views.py
@@ -5,12 +5,14 @@ from flask import flash, render_template, request, url_for, session
 from web_scraping import getLink
 from datetime import date
 import json
+
 nome = None
 imagem = None
 valor = 0
 today = date.today()
 
-@app.route('/', methods=['POST', 'GET',])
+
+@app.route('/', methods=['POST', 'GET', ])
 def index():
     data = today.strftime("%d/%m")
 
@@ -24,23 +26,26 @@ def index():
     dados = json.load(personagem)
 
     if data == dados['data']:
+        print(dados['nome'],dados['imagem'])
         nome = dados['nome']
         imagem = dados['imagem']
-
     else:
         with open('lista.json', 'w') as f:
             session['pontos'] = 5
             session['blur'] = 25
-            json.dump({'data': data,
-                       'nome': nome,
-                       'imagem': imagem}, f, ensure_ascii=False)
+            try:
+                json.dump({'data': data,
+                           'nome': nome,
+                           'imagem': imagem}, f, ensure_ascii=False)
+            except:
+                pass
 
     return render_template("template.html",
                            personagem=nome,
                            imagem=imagem)
 
 
-@app.route('/autenticar', methods=['POST', 'GET',])
+@app.route('/autenticar', methods=['POST', 'GET', ])
 def autenticar():
     if request.method == 'POST':
         entrada = " " + request.form['entrada'] + " "
